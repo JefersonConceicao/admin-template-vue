6124651c0b438ed65b675306e577e41fbaf8323b
Jeferson Conceicao de Oliveira <jefersonmallone2000@outlook.com>
2022-12-20 17:17:00 -0300

    first commit

---
+++ resources/App.vue
@@ -0,0 +1,19 @@
+<template>
+<div id="app">
+    <preloader v-show="this.$store.state.preloader"></preloader>
+    <router-view></router-view>
+</div>
+</template>
+<script>
+import preloader from "./components/layout/preloader";
+
+export default {
+    name: 'app',
+    components: {
+        preloader
+    }
+}
+</script>
+<style lang="scss" src="./sass/bootstrap/bootstrap.scss"></style>
+<style src="font-awesome/css/font-awesome.min.css"></style>
+<style src="themify-icons/css/themify-icons.css"></style>
